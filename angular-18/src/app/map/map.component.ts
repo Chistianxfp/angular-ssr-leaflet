300b45f6ac8f105429261b0918c7d35fc3fbf43e
Cristian Xavier Fieschi Ponce <cxfieschi@romeu.com>
2024-09-16 13:57:39 +0200

    Creating and showing the map

--- angular-18/src/app/map/map.component.ts
+++ angular-18/src/app/map/map.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject, PLATFORM_ID } from '@angular/core';
+import { LeafletService } from '../services/leaflet.service';
 
 @Component({
   selector: 'app-map',
@@ -8,5 +9,21 @@ import { Component } from '@angular/core';
   styleUrl: './map.component.scss'
 })
 export class MapComponent {
+  private leafletService = inject(LeafletService);
+  private L = this.leafletService.L;
+  map: MapComponent | undefined;
 
+  ngOnInit(){
+    if(this.L){
+      this.map = this.L.map('map', {center: [51.505, -0.09], zoom: 3});
+
+      const tiles = this.L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 18,
+        minZoom: 3,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      });
+
+      tiles.addTo(this.map);
+    }
+  }
 }
